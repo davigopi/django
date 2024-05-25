59f1ab41f481603700a8c445faddeafdcc7c6036
davigopi <davigopi@gmail.com>
2024-05-25 17:52:52 -0300

    2505

---
+++ calcular.py
@@ -0,0 +1,13 @@
+valor = 10
+valor = input('digite um valor: ')
+valor = float(valor)
+avista = valor * 0.9
+cartao_avista = valor * 0.95
+duas_cartao = valor *0.2
+tres_cartoa = (valor*1.2)*0.3 
+print(f'O valor digitado: {valor}')
+print(f'O valor avista: {avista}')
+print(f'O valor avista no cartão: {cartao_avista}')
+print(f'Duas vezes no cartao: {duas_cartao}')
+print(f'Tres vezes no cartão: {tres_cartoa}')
+
